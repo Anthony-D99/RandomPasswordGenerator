cf7492f43df0270cb79ca2b3969b0c5403e04eb5
Anthony-D99 <anthonydisaf@yahoo.com>
2023-04-14 18:00:58 -0400

    added variables for each prompt that is required as well as created an array for each character option for the password

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+var includeUpperCaseLetters = prompt("Would you like to include upper case letters in your password?")
+var includeLowerCaseLetters = prompt("Would you like to include lower case letters in your password?")
+var includeSpecialCharacters = prompt("Would you like to include special characters in your password?")
+var includeNumbers = prompt("Would you like to include numbers in your password?")
+var numberOptions = ["1","2","3","4","5","6","7","8","9","0"]
+var upperCaseOptions = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] 
+var lowerCaseOptions = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] 
+var specialCharacterOptions = ["]","[","!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","^","_","`","{","|","}","~","'",'"']

5c32abd8db48788f781271f63d4b4b95ffec7730
Anthony-D99 <anthonydisaf@yahoo.com>
2023-04-18 16:12:41 -0400

    Added all missing pieces that are needed to solve the challenge

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
 // Assignment Code
 var generateButton = document.querySelector("#generate");
-console.log(generateButton)
+var numberOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharacterOptions = ["]", "[", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~", "'", '"'];
+var password = [];
+
+function getRandomNumber(min, max) {
+  var randomNumber = Math.random()
+  var randomNumberUpToMax = randomNumber * max
+  var randomNumberInRange = min + randomNumberUpToMax;
+
+  return Math.floor(randomNumberInRange);
+
+}
+function getRandomValueArray(array) {
+  var randomArrayPosition = getRandomNumber(0, array.length);
+  return array[randomArrayPosition];
+}
+
+
 function generatePassword() {
-  var numberOfCharacters = prompt("How many character would you like your password to contain?")
-  var includeUpperCaseLetters = confirm("Would you like to include upper case letters in your password?")
-  var includeLowerCaseLetters = confirm("Would you like to include lower case letters in your password?")
-  var includeSpecialCharacters = confirm("Would you like to include special characters in your password?")
-  var includeNumbers = confirm("Would you like to include numbers in your password?")
-  // var passwordArray = []
+  
+  var generatedPassword= "";
 
+  var numberOfCharacters = prompt("How many character would you like your password to contain?");
 
-  // ask user criteria of password
-  // create empty array variable for storing password values
-  // generate a random number
-  // index option array to get random value
-  // repeat generation based on user criteria 
-  // use join method to combine password array variable
-  // return joined password
+  if(numberOfCharacters < 8 || numberOfCharacters > 128) {
+    alert("Password must be longer than 8 or less than 128 characters");
+    return;
+  }
 
+  var includeUpperCaseLetters = confirm("Would you like to include upper case letters in your password?");
+
+  if(includeUpperCaseLetters){
+    password = password.concact(upperCaseOptions);
+  }
+
+  var includeLowerCaseLetters = confirm("Would you like to include lower case letters in your password?");
+
+  if(includeLowerCaseLetters){
+    password = password.concact(lowerCaseOptions);
+  }
+
+  var includeSpecialCharacters = confirm("Would you like to include special characters in your password?");
+
+  if(includeSpecialCharacters){
+    password = password.concact(specialCharacterOptions);
+  }
+
+  var includeNumbers = confirm("Would you like to include numbers in your password?");
+
+  if(includeNumbers){
+    password = password.concact(numberOptions);
+  }
+ 
+ for(i = 0; i < numberOfCharacters; i++){
+  generatedPassword += getRandomValueArray(password);
+ }
+
+return generatedPassword;
 
 }
 // Write password to the #password input
@@ -28,17 +70,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
-function getRandomLetter() {
-  var letters = ['a', 'b']
-  return letters[getRandom(0, getRandomLetter.length)]
-}
+
 // Add event listener to generate button
 generateButton.addEventListener("click", writePassword);
 
 
 
 
-var numberOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var specialCharacterOptions = ["]", "[", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~", "'", '"']
